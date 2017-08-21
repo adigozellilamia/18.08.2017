61265cf6cda4343e885cb0277a1e8f37861d3cd1
adigozellilamia <lamiye.adigozelli@icloud.com>
2017-08-21 18:31:33 +0400

    ///

---
+++ 18.07.2017/js/script.js
@@ -0,0 +1,28 @@
+var button=document.querySelectorAll(".button");
+var cities=document.querySelector(".cities");
+var text=document.querySelectorAll(".text");
+
+for(var i=0; i<cities.length;i++);
+
+button.addEventListener("click",function(){
+
+    if(document.querySelector(".button .active")!=null){
+
+        if(document.querySelector(".button .active")!=this.nextElementSibling){
+
+            document.querySelector(".button .active").classList.remove(".active");
+
+        }
+ 
+    }
+ 
+    this.nextElementSibling.classList.toggle("active");
+});
+
+for(var i=0;i<city.length;i++);
+button.addEventListener("click",function(){
+    if(document.querySelector(".text").style.display="none");
+    document.getElementsByClassName("text").style.display="block";
+})
+    
+
